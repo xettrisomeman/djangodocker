bc509605c8ff85fa3e8859d79a1ae0b024d1efa4
webizfabulous <webizmoxx@gmail.com>
2019-12-11 11:10:16 +0545

    made changes to root urls and added a new url file inside app directory

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from djangoapp import views
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('djangoapp.urls'),
 ]
+
